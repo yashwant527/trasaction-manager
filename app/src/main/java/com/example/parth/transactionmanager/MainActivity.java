6e2117b5d3ba4c97c795b7aeb24667310133ca6e
Parth Rathore <parth.rathore15@gmail.com>
2018-07-09 18:35:14 +0530

    Added fab to open newTransaction activity

--- app/src/main/java/com/example/parth/transactionmanager/MainActivity.java
+++ app/src/main/java/com/example/parth/transactionmanager/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.parth.transactionmanager;
 
+import android.animation.Animator;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewAnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FloatingActionButton mFabAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, NewTransactionActivity.class);
-        startActivity(intent);
+        mFabAdd = findViewById(R.id.main_fab_add_transaction);
+
+        mFabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewTransactionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
