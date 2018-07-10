02b05a10cdc7ce81d2b9aae758507be131df7ba5
Parth Rathore <parth.rathore15@gmail.com>
2018-07-10 18:38:27 +0530

    Made entry class

---
+++ app/src/main/java/com/example/parth/transactionmanager/Entry.java
@@ -0,0 +1,75 @@
+package com.example.parth.transactionmanager;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Entry {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "tid")
+    private int mTid;
+
+    @NonNull
+    @ColumnInfo(name = "from")
+    private String mFrom;
+
+    @NonNull
+    @ColumnInfo(name = "to")
+    private String mTo;
+
+    @ColumnInfo(name = "description")
+    private String mDescription;
+
+    @ColumnInfo(name = "amount")
+    private double mAmount;
+
+    @ColumnInfo(name = "wallet")
+    private boolean mWallet;
+
+    @ColumnInfo(name = "need")
+    private boolean mNeed;
+
+
+    public Entry(@NonNull String from, @NonNull String to, String description,
+                 @NonNull double amount, @NonNull boolean wallet, @NonNull boolean need) {
+        this.mFrom = from;
+        this.mTo = to;
+        this.mDescription = description;
+        this.mAmount = amount;
+        this.mWallet = wallet;
+        this.mNeed = need;
+    }
+
+    public int getTid() {
+        return mTid;
+    }
+
+    @NonNull
+    public String getFrom() {
+        return mFrom;
+    }
+
+    @NonNull
+    public String getTo() {
+        return mTo;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public double getAmount() {
+        return mAmount;
+    }
+
+    public boolean isWallet() {
+        return mWallet;
+    }
+
+    public boolean isNeed() {
+        return mNeed;
+    }
+}
